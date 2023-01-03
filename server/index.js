4a5d27707ec9a4ba3e7f07f172c793f4c5ebe8f9
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-01-03 14:19:58 -0300

    Obtener Tarea y obtener tareas, rutas y controller simple

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 //console.log('Hola Niag'); 
-import express from 'express';
-import {PORT} from './config.js'
-import indexRoutes from './routes/index.routes.js' 
+import express from 'express'; 
+import {PORT} from './config.js'; 
+import indexRoutes from './routes/index.routes.js'; 
+import taskRoute from './routes/tasks.routes.js'; 
 
 const app = express(); 
 
-app.use(indexRoutes)
+app.use(indexRoutes); 
+app.use(taskRoute); 
 app.listen(PORT)
 console.log('Servidor ejecutandose en el puerto '+PORT+"."); 
