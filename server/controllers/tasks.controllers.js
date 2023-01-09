e105f56994c0fd1872e03b5b146e85fa1b97d5c9
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-01-09 14:56:57 -0300

    Listando tareas desde la BD y con mensaje en postman

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,19 +1,24 @@
 import { pool } from '../db.js'; 
 
+export const getTasks = async (req, res) => { 
+    //res.send('Obteniendo tareas.'); 
+    const [result] = await pool.query("SELECT * FROM tasks ORDER BY createdAt ASC"); 
+    res.json(result);
+} 
+
+export const getTask = (req, res) => { 
+    res.send('Obteniendo tarea.'); 
+} 
+
 export const createTask = async (req, res) => { 
     const { title, description } = req.body;
     const [result] = await pool.query("INSERT INTO tasks (title, description) VALUES (?, ?)", [title, description]);
     console.log(result);
     res.json({id: result.insertId, title, description});
     //console.log(req.body); 
-    res.send('Creando tarea.'); } 
+    res.send('Creando tarea.');
+} 
 
-    export const getTasks = (req, res) => { 
-        res.send('Obteniendo tareas.'); 
-    } 
-    export const getTask = (req, res) => { 
-        res.send('Obteniendo tarea.'); 
-    } 
 export const updateTask = (req, res) => { 
     res.send('Actualizando tarea.'); 
 } 
