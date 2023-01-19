bf278a5b9a8962c15c2c92764bf92be58ef04eed
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-01-19 17:59:03 -0300

    Listar desde el front-end

--- client/src/api/Task.api.js
+++ client/src/api/Task.api.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
 //Funcion para enviar datos 
+
+export const getTaskRequest = async () => 
+    await axios.get('http://localhost:4000/tasks'); 
+
 export const createTaskRequest = async (task) => 
-    await axios.post('http://localhost:4000/tasks', task)
+    await axios.post('http://localhost:4000/tasks', task); 
--- client/src/pages/TasksPage.js
+++ client/src/pages/TasksPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import { getTaskRequest } from '../api/Task.api';
+import TaskCard from '../components/TaskCard.js';
 
 function TasksPage(){
-    return (
-      <div>Tasks Page.</div>
-    )
+
+  const [tasks,setTasks] = useState([]); 
+
+  useEffect(() => {
+    async function loadTasks(){
+      const response = await getTaskRequest();
+      //console.log(response.data);
+      setTasks(response.data);
+    }
+    loadTasks()
+  }, [])
+
+  return (
+    //<></>
+    <div>
+      <h1>Tasks</h1>
+      {
+        tasks.map(task => (
+          <TaskCard task={task} key={task.id}/>
+        ))
+      }
+    </div>
+  )
 }
 
 export default TasksPage;
