49786417016aa2165044c044fd796aa108ace305
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-01-20 15:27:44 -0300

    Eliminando desde front-end

--- client/src/pages/TasksPage.js
+++ client/src/pages/TasksPage.js
@@ -3,9 +3,7 @@ import { getTaskRequest } from '../api/Task.api';
 import TaskCard from '../components/TaskCard.js';
 
 function TasksPage(){
-
   const [tasks,setTasks] = useState([]); 
-
   useEffect(() => {
     async function loadTasks(){
       const response = await getTaskRequest();
@@ -13,17 +11,20 @@ function TasksPage(){
       setTasks(response.data);
     }
     loadTasks()
-  }, [])
+  }, []); 
+
+  function renderMain(){
+    if (tasks.length === 0) return <h1>No tasks yet.</h1>
+      return tasks.map((task) => 
+        <TaskCard task={task} key={task.id} />);
+  }
 
   return (
     //<></>
     <div>
       <h1>Tasks</h1>
-      {
-        tasks.map(task => (
-          <TaskCard task={task} key={task.id}/>
-        ))
-      }
+      {renderMain()}
+      
     </div>
   )
 }
